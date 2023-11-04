ccaa148108c4f85d1f4831534d2b350e1b0792ec
Dan Wilkins <danwilkins2003@gmail.com>
2023-11-04 12:36:30 +0000

    Block Created

--- GameStart.js
+++ GameStart.js
@@ -16,21 +16,33 @@ let gameCanvas = {
 };
 
 let player;
+let block;
 
 function startGame() {
-    gameCanvas.start();
-    player = new createPlayer(30, 30, 10, 120);
+  gameCanvas.start();
+  player = new createPlayer(30, 30, 10, 120);
+
+  block = new createBlock(10, 10);
 }
 
 function createPlayer(width, height, x, y) {
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y; 
-    
-    ctx = gameCanvas.context;
-    ctx.fillStyel = "green";
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+
+  ctx = gameCanvas.context;
+  ctx.fillStyel = "green";
+  ctx.fillRect(this.x, this.y, this.width, this.height);
 }
 
+function createBlock(width, height) {
+  this.width = width;
+  this.height = height;
+  this.x = canvasWidth - 100;
+  this.y = Math.floor(Math.random() * canvasHeight);
 
+  ctx = gameCanvas.context;
+  ctx.fillStyle = "red";
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+}
