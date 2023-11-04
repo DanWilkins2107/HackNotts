82e28d5ac6e2639ca10446a8e3cbbe7a73ff88ce
Dylan Wulfse <dylan@wulfse.com>
2023-11-04 13:54:47 +0000

    Adding player movement

--- GameStart.js
+++ GameStart.js
@@ -56,23 +56,54 @@ function createPlayer(width, height, x, y) {
     ctx.fillStyle = "green";
     ctx.fillRect(this.x, this.y, this.width, this.height);
   };
-
-  ctx = gameCanvas.context;
-  ctx.fillStyle = "green";
-  ctx.fillRect(this.x, this.y, this.width, this.height);
 }
 
+let keyMap = {
+  left: ["a", "ArrowLeft"],
+  right: ["d", "ArrowRight"],
+  up: ["w", "ArrowUp"],
+  down: ["s", "ArrowDown"],
+};
+let keysPressed = {};
 function createBlock(width, height) {
   this.width = width;
   this.height = height;
   this.x = canvasWidth + 100;
   this.y = Math.floor(Math.random() * canvasHeight - 20) + 10;
 
+document.addEventListener("keydown", (event) => {
+  keysPressed[event.key] = true;
   this.draw = function () {
     ctx = gameCanvas.context;
     ctx.fillStyle = "red";
     ctx.fillRect(this.x, this.y, this.width, this.height);
   };
+
+  if ((keyMap.left.includes(event.key) || keysPressed[keyMap.left[0]] || keysPressed[keyMap.left[1]]) && player.x > 0) {
+    player.x -= 10;
+  }
+  if ((keyMap.right.includes(event.key) || keysPressed[keyMap.right[0]] || keysPressed[keyMap.right[1]]) && player.x < canvasWidth - player.width) {
+    player.x += 10;
+  }
+  if ((keyMap.up.includes(event.key) || keysPressed[keyMap.up[0]] || keysPressed[keyMap.up[1]]) && player.y > 0) {
+    player.y -= 10;
+  }
+  if ((keyMap.down.includes(event.key) || keysPressed[keyMap.down[0]] || keysPressed[keyMap.down[1]]) && player.y < canvasHeight - player.height) {
+    player.y += 10;
+  }
+  updateCanvas();
+});
+
+document.addEventListener("keyup", (event) => {
+  delete keysPressed[event.key];
+});
+
+function updateCanvas() {
+  ctx = gameCanvas.context;
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+  player.draw();
+}
 
   this.delete = function () {
     if (this.x < -100) {
